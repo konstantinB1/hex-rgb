576d1ff75fabac0d97ccae61a6e2931ba6ead7b4
Konstantin Bulatovic <konstantinbulatovic1@gmail.com>
2019-12-25 10:20:23 +0100

    hex-rgb package es5 code

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 'use strict';
 
-const hexCharacters = 'a-f\\d';
-const match3or4Hex = `#?[${hexCharacters}]{3}[${hexCharacters}]?`;
-const match6or8Hex = `#?[${hexCharacters}]{6}([${hexCharacters}]{2})?`;
-const nonHexChars = new RegExp(`[^#${hexCharacters}]`, 'gi');
-const validHexSize = new RegExp(`^${match3or4Hex}$|^${match6or8Hex}$`, 'i');
+var hexCharacters = 'a-f\\d';
+var match3or4Hex = "#?[".concat(hexCharacters, "]{3}[").concat(hexCharacters, "]?");
+var match6or8Hex = "#?[".concat(hexCharacters, "]{6}([").concat(hexCharacters, "]{2})?");
+var nonHexChars = new RegExp("[^#".concat(hexCharacters, "]"), 'gi');
+var validHexSize = new RegExp("^".concat(match3or4Hex, "$|^").concat(match6or8Hex, "$"), 'i');
 
-module.exports = (hex, options = {}) => {
-	if (typeof hex !== 'string' || nonHexChars.test(hex) || !validHexSize.test(hex)) {
-		throw new TypeError('Expected a valid hex string');
-	}
+module.exports = function (hex) {
+  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-	hex = hex.replace(/^#/, '');
-	let alpha = 1;
+  if (typeof hex !== 'string' || nonHexChars.test(hex) || !validHexSize.test(hex)) {
+    throw new TypeError('Expected a valid hex string');
+  }
 
-	if (hex.length === 8) {
-		alpha = parseInt(hex.slice(6, 8), 16) / 255;
-		hex = hex.slice(0, 6);
-	}
+  hex = hex.replace(/^#/, '');
+  var alpha = 1;
 
-	if (hex.length === 4) {
-		alpha = parseInt(hex.slice(3, 4).repeat(2), 16) / 255;
-		hex = hex.slice(0, 3);
-	}
+  if (hex.length === 8) {
+    alpha = parseInt(hex.slice(6, 8), 16) / 255;
+    hex = hex.slice(0, 6);
+  }
 
-	if (hex.length === 3) {
-		hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-	}
+  if (hex.length === 4) {
+    alpha = parseInt(hex.slice(3, 4).repeat(2), 16) / 255;
+    hex = hex.slice(0, 3);
+  }
 
-	const num = parseInt(hex, 16);
-	const red = num >> 16;
-	const green = (num >> 8) & 255;
-	const blue = num & 255;
+  if (hex.length === 3) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
 
-	return options.format === 'array' ?
-		[red, green, blue, alpha] :
-		{red, green, blue, alpha};
+  var num = parseInt(hex, 16);
+  var red = num >> 16;
+  var green = num >> 8 & 255;
+  var blue = num & 255;
+  return options.format === 'array' ? [red, green, blue, alpha] : {
+    red: red,
+    green: green,
+    blue: blue,
+    alpha: alpha
+  };
 };
